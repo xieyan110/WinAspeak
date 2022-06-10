fa6c2521f5f0253b18fdd931fa74dd9a88f1d59d
xieyan110 <xieyan119@yahoo.com>
2022-06-10 17:09:07 +0800

    提交

--- WinAspeak/WinAspeak.Designer.cs
+++ WinAspeak/WinAspeak.Designer.cs
@@ -67,6 +67,9 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Location = new System.Drawing.Point(3, 0);
             this.groupBox2.Name = "groupBox2";
